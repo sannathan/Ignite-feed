4e8ce8dafe22bbf32086a7c7c53562bcd97202ff
Nathan Barbosa <nathansantos1713@gmail.com>
2025-01-24 12:18:41 -0300

    feature(main): add hover on button, profile image and finished the sidebar.

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,3 +1,4 @@
+import {PencilLine } from 'phosphor-react'
 import styles from "./sidebar.module.css";
 
 export function Sidebar() {
@@ -8,10 +9,13 @@ export function Sidebar() {
         src="https://plus.unsplash.com/premium_photo-1678565869434-c81195861939?w=400&auto=format&fit=crop&q=40&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww"
       />
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/sannathan.png"/>
         <strong>Nathan Barbosa</strong>
         <span>Web Developer</span>
         <footer>
-          <a href="#">Editar seu perfil</a>
+          <a href="#">
+            <PencilLine size={20}/>
+            Editar seu perfil</a>
         </footer>
       </div>
     </aside>
