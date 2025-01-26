5f1e014220369bf665e172fdfe484c8b6e2fba76
Nathan Barbosa <nathansantos1713@gmail.com>
2025-01-26 12:26:52 -0300

    feature(main): post html done, started de post stylization

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from "./components/header";
-import { Post } from "./post";
+import { Post } from "./components/post";
+import { Sidebar } from "./components/sidebar";
 import "./global.css";
 
 import styles from "./App.module.css";
-import { Sidebar } from "./components/sidebar";
 
 export function App() {
   return (
---
+++ src/components/post.jsx
@@ -0,0 +1,37 @@
+import styles from "./post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://github.com/sannathan.png" />
+          <div className={styles.authorInfo}>
+            <strong>Nathan Barbosa</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="25 de Janeiro as 15:08" dateTime="2025-01-25 15:07:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉{' '} <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
