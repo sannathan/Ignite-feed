80ccb09a62e2b102e75a5dfc1bc7e7485c1b8dfb
Nathan Barbosa <nathansantos1713@gmail.com>
2025-01-29 08:28:39 -0300

    feature(main): page done

---
+++ src/components/comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/sannathan.png" alt="" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Nathan Barbosa</strong>
+                            <time datetime="11 de Maio às 08:13h" dateTime='2024-01-28 08:09:30'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Muito bom Nathan, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
