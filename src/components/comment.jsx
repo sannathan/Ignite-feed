7c48f27c93288ae62849c0f148f634727133db6d
Nathan Barbosa <nathansantos1713@gmail.com>
2025-02-07 13:25:11 -0300

    feature(main): add delete comments function

--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -1,32 +1,41 @@
-import { ThumbsUp, Trash } from 'phosphor-react';
-import styles from './comment.module.css';
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./comment.module.css";
 
-export function Comment({ content }) {
-    return (
-        <div className={styles.comment}>
-            <img src="https://github.com/sannathan.png" alt="" />
-            <div className={styles.commentBox}>
-                <div className={styles.commentContent}>
-                    <header>
-                        <div className={styles.authorAndTime}>
-                            <strong>Nathan Barbosa</strong>
-                            <time datetime="11 de Maio às 08:13h" dateTime='2024-01-28 08:09:30'>Cerca de 1h atrás</time>
-                        </div>
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
 
-                        <button title='Deletar comentário'>
-                            <Trash size={24} />
-                        </button>
-                    </header>
-                    <p>{content}</p>
-                </div>
-
-                <footer>
-                    <button>
-                        <ThumbsUp />
-                        Aplaudir <span>20</span>
-                    </button>
-                </footer>
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/sannathan.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Nathan Barbosa</strong>
+              <time
+                datetime="11 de Maio às 08:13h"
+                dateTime="2024-01-28 08:09:30"
+              >
+                Cerca de 1h atrás
+              </time>
             </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
         </div>
-    )
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
